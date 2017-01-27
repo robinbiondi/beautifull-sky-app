12d27d6c1dec12d31f75cef520243218524f0a92
robin biondi <robin.bion@gmail.com>
2017-01-27 17:30:33 +0100

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 var gulp = require('gulp');
+var http = require('http');
+var st   = require('st');
 
 gulp.task('default', function() {
-  
+	http.createServer(st({
+    path : __dirname + '/beautifull-sky/www',
+    index: 'index.html',
+    cache: false,
+  })).listen(8000);  
 });
